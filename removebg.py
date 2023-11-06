02373b5f73b9dd0388efc89624654eaf0f4a18d3
楊硯鈞 <91245606+yenjun@users.noreply.github.com>
2023-11-06 16:39:14 +0800

    Create removebg.py

---
+++ removebg.py
@@ -0,0 +1,12 @@
+from rembg import remove
+from PIL import Image
+import cv2
+from google.colab.patches import cv2_imshow
+
+input_path = 'Jack_new.png'
+output_path = 'output.png'
+input = Image.open(input_path)
+output = remove(input)
+output.save(output_path)
+img = cv2.imread('output.png')
+cv2_imshow(img)
